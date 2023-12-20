8f4deb3bd942aa0bb1111a9226de5c74f7b04b14
Phelyks <flexxmosh@gmail.com>
2023-12-20 10:44:49 +0300

    update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,10 +8,16 @@ const Hero = () => {
     <div className="hero">
         <div className="left-h">
             <Header/>
+            {/* The best add */}
             <div className="the-best-ad">
                 <div></div>
                 <span>the best fitness club in the town</span>
             </div>
+            {/* The hero heading */}
+        <div className="hero-text">
+          <div><span className='stroke-text'>Shape </span><span>Your</span></div>
+          <div><span>Ideal body</span></div>
+        </div>
         </div>
         <div className="right-h">right-side</div>
     </div>
