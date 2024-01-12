428d328812f718c8e04acef6493191c09e1753cc
Phelyks <flexxmosh@gmail.com>
2024-01-12 10:57:15 +0300

    Jan 12 10:57

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -5,7 +5,11 @@ import './Testimonials.css';
 const Testimonials = () => {
   return (
     <div className='Testimonials'>
-      <div className="left-t"></div>
+      <div className="left-t">
+        <span>Testimonials </span>
+        <span className='stroke-text'>What they </span>
+        <span>say about us</span>
+      </div>
       <div className="right-t"></div>
     </div>
   )
