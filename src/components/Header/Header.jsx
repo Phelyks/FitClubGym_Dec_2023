53529abda7c0c5369dbb004c99ada592b7beb567
Phelyks <flexxmosh@gmail.com>
2024-01-18 14:16:55 +0300

    Jan18 14:16

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import Logo from './../../assets/logo.png';
+import Bars from './../../assets/bars.png';
+import Link from 'react-scroll';
 
 const Header = () => {
+
+  const mobile = window.innerWidth <= 768 ? true : false;
+
+  const [menuOpened, setMenuOpened] = useState(false);
+
   return (
     <div className='header'>
       <img src={Logo} alt="" className='logo' />
-        <ul className='header-menu'>
-            <li>Home</li>
-            <li>Programs</li>
-            <li>Why Us</li>
-            <li>Plans</li>
-            <li>Testimonials</li>
-        </ul>
+
+      {(menuOpened === false && mobile === true) ? (
+        <div 
+        
+        style={{
+          backgroundColor:'var(--appColor)',
+          padding:'0.5rem',
+          borderRadius:'5px',
+        }}
+
+        onClick={()=> setMenuOpened(true)}
+
+        ><img src={Bars} alt="" style={{width:'1.5rem', height:'1.5rem',}} /></div>
+      ): <ul className='header-menu'>
+      <li onClick={()=> setMenuOpened(false)}> <Link to='' >Home</Link></li>
+      <li onClick={()=> setMenuOpened(false)}> <Link to='' >Programs</Link></li>
+      <li onClick={()=> setMenuOpened(false)}> <Link to='' >Why Us</Link></li>
+      <li onClick={()=> setMenuOpened(false)}> <Link to='' >Plans</Link></li>
+      <li onClick={()=> setMenuOpened(false)}> <Link to='testimonials'
+                                                          span={true}
+                                                          smooth={true}
+                                                          >Testimonials</Link></li>
+        </ul>}
+        
 
     </div>
   )
